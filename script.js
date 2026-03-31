7f02d3d884ee729165bb09fce69315fadec98c0d
ceebookanswers <examclub10@gmail.com>
2026-03-31 20:02:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ function checkPassword() {
     var password = document.getElementById("password").value.trim();
 
     if (password === "0000") {
-        window.location.href = "./math.html";
+        window.location.href = "https://www.ceebookanswers.com.ng/p/waec-mathematics-2025.html?m=1";
     } 
     else if (password === "567") {
-        window.location.href = "https://www.ceebookanswers.com.ng/2021/01/how-to-subscribe100.html?m=1";
+        window.location.href = "./maths.html";
     } 
     else if (password === "1") {
-        window.location.href = "./ans1.html";
+        window.location.href = "https://www.ceebookanswers.com.ng/p/lite.html?m=1";
     } 
     else if (password === "222") {
-        window.location.href = "./ans2.html";
+        window.location.href = "https://www.ceebookanswers.com.ng/p/neco-chemistry.html?m=1";
     } 
     else if (password === "333") {
-        window.location.href = "./ans3.html";
+        window.location.href = "https://www.ceebookanswers.com.ng/p/waec-2025-english-language.html?m=1";
     } 
     else {
         alert("Wrong Password! Please contact Mr Dan.");
