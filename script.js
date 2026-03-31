e5f4e2993e30d326e566b1ddc7fa700e85b1b6e5
ceebookanswers <examclub10@gmail.com>
2026-03-31 05:39:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function checkPassword() {
         alert("Wrong Password! Please contact Mr Dan.");
     }
 }
+
+
+
+    } 
+    else if(password === "567") {
+        window.location.href = "ans1.html";
+    } 
+    else if(password === "bio123") {
+        window.location.href = "biology.html";
+    } 
+    else if(password === "gov123") {
+        window.location.href = "government.html";
+    } 
+     else if(password === "gov123") {
+        window.location.href = "government.html";
+    } 
+
+
+    } 
+    else if(password === "567") {
+        window.location.href = "ans";
+    } 
+    else if(password === "bio123") {
+        window.location.href = "biology.html";
+    } 
+    else if(password === "gov123") {
+        window.location.href = "government.html";
+    } 
