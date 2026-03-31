a400acb690ad961c350955a1eb54113f6a8911ac
ceebookanswers <examclub10@gmail.com>
2026-03-31 05:55:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,22 @@
 function checkPassword() {
     var password = document.getElementById("password").value;
 
-    if(password === "123") {
+    if (password === "123") {
         window.location.href = "math.html";
     } 
-    else if(password === "567") {
+    else if (password === "567") {
         window.location.href = "https://www.ceebookanswers.com.ng/2021/01/how-to-subscribe100.html?m=1";
     } 
-    else if(password === "bio123") {
+    else if (password === "bio123") {
         window.location.href = "biology.html";
     } 
-    else if(password === "gov123") {
+    else if (password === "gov123") {
         window.location.href = "government.html";
+    } 
+    else if (password === "ans1") {
+        window.location.href = "Ans1.html";
     } 
     else {
         alert("Wrong Password! Please contact Mr Dan.");
     }
 }
-
-
-
-    } 
-    else if(password === "ans1") {
-        window.location.href = "Ans1.html";
-    } 
-    else if(password === "bio123") {
-        window.location.href = "biology.html";
-    } 
-    else if(password === "gov123") {
-        window.location.href = "government.html";
-    } 
-     else if(password === "gov123") {
-        window.location.href = "government.html";
-    } 
-
-
-    } 
-    else if(password === "567") {
-        window.location.href = "ans";
-    } 
-    else if(password === "bio123") {
-        window.location.href = "biology.html";
-    } 
-    else if(password === "gov123") {
-        window.location.href = "government.html";
-    } 
