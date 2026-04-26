310a7f367582b7d515b54927c1d17412e10f47e0
ceebookanswers <examclub10@gmail.com>
2026-04-26 12:47:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function checkPassword() {
     var password = document.getElementById("password").value.trim();
 
-    if (password === "000") {
-        window.location.href = "https://www.ceebookanswers.com.ng/p/waec-mathematics-2025.html?m=1";
-    } 
-    else if (password === "567") {
-        window.location.href = "Copy.html";
-    } 
-    else if (password === "111") {
-        window.location.href = "https://www.ceebookanswers.com.ng/p/lite.html?m=1";
-    } 
-    else if (password === "222") {
-        window.location.href = "https://www.ceebookanswers.com.ng/p/neco-chemistry.html?m=1";
-    } 
-    else if (password === "333") {
-        window.location.href = "https://www.ceebookanswers.com.ng/p/waec-2025-english-language.html?m=1";
-    } 
-    else {
+    var routes = {
+        "000": "https://www.ceebookanswers.com.ng/p/waec-mathematics-2025.html?m=1",
+        "567": "Copy.html",
+        "111": "https://www.ceebookanswers.com.ng/p/lite.html?m=1",
+        "222": "https://www.ceebookanswers.com.ng/p/neco-chemistry.html?m=1",
+        "333": "https://www.ceebookanswers.com.ng/p/waec-2025-english-language.html?m=1",
+        "444": "page4.html",
+        "555": "page5.html",
+        "666": "page6.html",
+        "777": "page7.html",
+        "888": "page8.html"
+    };
+
+    if (routes[password]) {
+        window.location.href = routes[password];
+    } else {
         alert("Wrong Password! Please contact Mr Dan.");
     }
 }
