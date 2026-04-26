6c38634f7da02bd40f41fc53d64bdb251c113fe8
ceebookanswers <examclub10@gmail.com>
2026-04-26 13:24:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function checkPassword() {
     var password = document.getElementById("password").value.trim();
 
     var routes = {
-        "000": "https://www.ceebookanswers.com.ng/p/waec-mathematics-2025.html?m=1",
-        "567": "Copy.html",
-        "111": "https://www.ceebookanswers.com.ng/p/lite.html?m=1",
-        "222": "https://www.ceebookanswers.com.ng/p/neco-chemistry.html?m=1",
-        "333": "https://www.ceebookanswers.com.ng/p/waec-2025-english-language.html?m=1",
+        "186": "page186.html",
+        "187": "page187.html",
+        "188": "page188.html",
+        "222": "page2.html",
+        "333": "page3.html",
         "444": "page4.html",
         "555": "page5.html",
         "666": "page6.html",
