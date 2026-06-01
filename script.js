44d24968e3d64444bd9166a7a781c77fe85665a8
ceebookanswers <examclub10@gmail.com>
2026-06-01 04:51:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function checkPassword() {
     var routes = {
         "945": "page945.html",
         "574": "page574.html",
-        "30": "page1.html",
+        "441": "page441.html",
         "31": "page2.html",
         "32": "page3.html",
         "310": "page310.html",
