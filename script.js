c5dc4339cb124e6329a64d2e54291f4c7c8f250c
ceebookanswers <examclub10@gmail.com>
2026-06-01 05:14:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,11 @@ function checkPassword() {
     var password = document.getElementById("password").value.trim();
 
     var routes = {
+        "954": "page954.html",
+        "944": "page944.html",
+        "441": "page441.html",
         "95": "page95.html",
-        "59": "page59.html",
-        "41": "page41.html",
-        "954": "page954.html,
-        "441": "page441.html,
-        "31": "page31.html",
-        "32": "page32.html",
-        "1": "page1.html",
-        "311": "page311.html",
-        "312": "page312.html",
-        "99": "page99.html",
-        "944": "page944.html"
+        "99": "page99.html"
     };
 
     if (routes[password]) {
